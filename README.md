8482d4d4ad695406a63a36418b657af0067204a2
Luke Woodward <woodward.lucas@gmail.com>
2014-08-22 21:20:25 -0700

    First commit for SVN co and up

---
+++ README.md
@@ -0,0 +1,81 @@
+# grunt-gitPull
+
+> Clone and Pull repos with Grunt
+
+## Getting Started
+This plugin requires Grunt.
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-gitPull --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-gitPull');
+```
+
+## The "gitPull" task
+
+### Overview
+In your project's Gruntfile, add a section named `gitPull` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  gitPull: {
+    set: {
+      repos: [
+        // array of object with relative path arrays and repo keys.
+      ]
+    },
+  },
+})
+```
+
+Now when running `grunt gitPull` the plugin will check all paths and see if there is a git repo in them. If so, it will run a git pull on that repository. If it is not present, it will run `git clone` with the repository specified.
+
+### Usage Example
+In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
+
+```js
+grunt.initConfig({
+  gitPull: {
+    example: {
+      repos: [
+        {
+          path: ['relative', 'path'], // relative/path/
+          repo: 'git@git.repository.com/myRepo.git'
+        },
+        {
+          path: ['another', 'path'] // another/path/
+          repo: 'git@git.repository.com/otherRepo.git'
+        }
+      ]
+    },
+  },
+})
+```
+
+## Release History
+**0.1.4**
+
+ - Support older version of git
+ 
+**0.1.2**
+
+ - Parse out the repo name rather than specifying it.
+
+**0.1.1**
+
+ - Report which repo is being processed.
+ - Fix README docs.
+
+**0.1.0**
+
+ - First release.
+ - Basic git clone/pull behavior.
+
+## License
+Copyright (c) 2014 Luke Woodward. Licensed under the MIT license.
---
+++ tasks/git-svn-checkout.js
@@ -0,0 +1,81 @@
+/*
+ * grunt-gitPull
+ * https://github.com/lkwdwrd/grunt-gitPull
+ *
+ * Copyright (c) 2014 Luke Woodward
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var path  = require('path'),
+    async = require( 'async' );
+
+
+
+module.exports = function (grunt) {
+  var SELF = this;
+
+  function svnCMD( cmd, repo ) {
+    return function( done ){
+      console.log( 'Processing ' + repo );
+      var process = grunt.util.spawn( { cmd: 'svn', args: cmd, opts: { stdio: 'inherit' } }, done );
+    };
+  }
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('svn-checkout', 'Checkout and update SVN repos with Grunt', function () {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var done = this.async(),
+        commands = [],
+        repos = this.data.repos,
+        relPath = {},
+        cmd, dir;
+
+    // Gather repo commands.
+    for ( var i = 0, length = repos.length; i < length; i++ ) {
+      // Make sure we have a repo
+      if ( ! repos[ i ].repo ) {
+        continue;
+      }
+
+      // parse the repo name
+      if ( repos[ i ].dir ) {
+        dir = repos[ i ].dir;
+      } else {
+        dir = repos[ i ].repo.match(/\/([^\/]+?)(?:.svn)?$/);
+        if ( ! dir[1] ) {
+          grunt.log.warn('There was some trouble parsing the repository ' + repos[ i ].repo );
+          continue;
+        }
+        dir = dir[1];
+      }
+
+      // Set up working paths.
+      relPath.outer = path.join.apply( this, repos[ i ].path );
+      relPath.inner = relPath.outer + path.sep + dir;
+      
+      // validate outer directory
+      if ( ! grunt.file.isDir( relPath.outer ) ) {
+        grunt.log.warn('The directory "' + relPath + '" not found.');
+        continue;
+      }
+      
+      // Set up pull or clone
+      if ( grunt.file.isDir( relPath.inner + path.sep  + '.svn' ) ) {
+        cmd = [ 'update', relPath.inner ];
+      } else {
+        if ( ! grunt.file.isDir( relPath.inner ) ) {
+          grunt.file.mkdir( relPath.inner );
+        }
+        cmd = [ 'checkout', repos[ i ].repo, relPath.inner ];
+      }
+      commands.push( svnCMD( cmd, repos[ i ].repo ) );
+    }
+    // run the commands in parallel.
+    async.parallel( commands, done );
+  });
+
+};
